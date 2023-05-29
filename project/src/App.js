b0960d9df0fada5ce7a13f057a3056625aa5e2ea
sajid awan <sajidawan@sajids-MacBook-Air-2.local>
2023-05-29 23:50:47 +0530

    good

--- project/src/App.js
+++ project/src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Alert from "./components/Alert";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+// 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
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode ha been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode ha been enabled", "success");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      
+        <div className="container my-3">
+          {/* <switch>
+            <Route path="/about">
+              <About />
+            </Route>
+
+            <Route path="/">
+              <TextForm
+                showAlert={showAlert}
+                heading="Enter The Text To Analyze"
+                mode={mode}
+              />
+            </Route>
+          </switch> */}
+
+           <TextForm showAlert={showAlert} heading="Enter The Text To Analyze" mode={mode} /> 
+        </div>
+      
+    </>
   );
 }
 
